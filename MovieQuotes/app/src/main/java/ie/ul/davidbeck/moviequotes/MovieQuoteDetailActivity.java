026791c706f66938d8d19050fc133284f9093801
David-Beck-UL <39083800+David-Beck-UL@users.noreply.github.com>
2018-11-09 00:25:17 +0000

    removed some error code

--- MovieQuotes/app/src/main/java/ie/ul/davidbeck/moviequotes/MovieQuoteDetailActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/davidbeck/moviequotes/MovieQuoteDetailActivity.java
@@ -53,15 +53,6 @@ public class MovieQuoteDetailActivity extends AppCompatActivity {
                 // TODO: Delete this quote and close this activity
                 return true;
         }
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
